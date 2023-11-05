7ac2a7f3320d080b1bfbe4495a98595cfe45eee0
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-11-05 17:28:41 +0700

    Update krathong.js

--- krathong.js
+++ krathong.js
@@ -1,5 +1,198 @@
 window.addEventListener("load", (event) => {
   let result = `
-`
-  document.getElementById("demo").innerHTML = result
+  <div class="bg-main bg-province1">
+				<div class="river">
+						<div id="divProvince1" style="">
+							<div class="item-relative">
+								<div class="items-slide-46 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-4 items-thumb">
+										<img src="./img/krathong.png" alt="กระทง0">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item-relative">
+								<div class="items-slide-57 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-0 items-thumb">
+										<img src="./img/krathong-1.png" alt="กระทง0">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item-relative">
+								<div class="items-slide-64 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-1 items-thumb">
+										<img src="./img/krathong-2.png" alt="กระทง1">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-47 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-2 items-thumb">
+										<img src="./img/krathong-3.png" alt="กระทง2">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item-relative">
+								<div class="items-slide-48 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-3 items-thumb">
+										<img src="./img/krathong-4.png" alt="กระทง3">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item-relative">
+								<div class="items-slide-49 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-4 items-thumb">
+										<img src="./img/krathong-5.png" alt="กระทง4">
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item-relative">
+								<div class="items-slide-50 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-0 items-thumb">
+										<img src="./img/krathong-6.png" alt="กระทง0">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-51 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-1 items-thumb">
+										<img src="./img/krathong-1.png" alt="กระทง1">
+									    </div>
+									</div>
+								</div>
+							</div>
+																				
+																				
+							<div class="item-relative">
+								<div class="items-slide-52 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-2 items-thumb">
+										<img src="./img/krathong-2.png" alt="กระทง2">
+									    </div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-53 item-base-slide " >
+									<div class="items-topdown">
+										<div class="items-swing item-3 items-thumb">
+										<img src="./img/krathong-3.png" alt="กระทง3">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+																				
+						 <div class="item-relative">
+								<div class="items-slide-54 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-4 items-thumb">
+										<img src="./img/krathong-4.png" alt="กระทง4">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-55 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-0 items-thumb">
+										<img src="./img/krathong-5.png" alt="กระทง0">
+																				</div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-56 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-1 items-thumb">
+										<img src="./img/krathong-6.png" alt="กระทง1">
+										</div>
+									</div>
+								</div>
+							</div>
+							
+							<div class="item-relative">
+								<div class="items-slide-58 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-1 items-thumb">
+										<img src="./img/krathong-1.png" alt="กระทง1">
+									    </div>
+									</div>
+								</div>
+							</div>
+																				
+																				
+							<div class="item-relative">
+								<div class="items-slide-59 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-2 items-thumb">
+										<img src="./img/krathong-2.png" alt="กระทง2">
+									    </div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-60 item-base-slide " >
+									<div class="items-topdown">
+										<div class="items-swing item-3 items-thumb">
+										<img src="./img/krathong-3.png" alt="กระทง3">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+																				
+						 <div class="item-relative">
+								<div class="items-slide-61 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-4 items-thumb">
+										<img src="./img/krathong-4.png" alt="กระทง4">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-62 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-0 items-thumb">
+										<img src="./img/krathong-5.png" alt="กระทง0">
+										</div>
+									</div>
+								</div>
+							</div>
+																				
+							<div class="item-relative">
+								<div class="items-slide-63 item-base-slide ">
+									<div class="items-topdown">
+										<div class="items-swing item-1 items-thumb">
+										<img src="./img/krathong-6.png" alt="กระทง1">
+										</div>
+									</div>
+								</div>
+							</div>																			
+						</div>
+				</div>
+		</div>`
+  document.getElementById("krathong").innerHTML = result
 });

a3b1eaee2ab6811c16d9cbf9b10a9b8e179b1080
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-11-06 23:25:24 +0700

    Update krathong.js

--- krathong.js
+++ krathong.js
@@ -7,7 +7,7 @@
 								<div class="items-slide item-base-slide ">
 									<div class="items-topdown">
 										<div class="items-swing item-0 items-thumb">
-										<img src="https://semicon.github.io/loykrathong/img/krathong.png" alt="กระทง0" style="width:200px; height: auto;">
+										<img id="krathongMain" src="https://semicon.github.io/loykrathong/img/krathong.png" alt="krathongMain" style="width:200px; height: auto;">
 										</div>
 									</div>
 								</div>

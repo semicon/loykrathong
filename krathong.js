9fec593c0571c334e13d913acc9be86efe11a4f2
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-11-19 11:58:13 +0700

    Update krathong.js

--- krathong.js
+++ krathong.js
@@ -227,8 +227,10 @@
 						</div>
 				</div>
 		</div>`
+document.write(`
 const ex = document.querySelector("body");
 const linkx = ex.innerHTML.match(`<script src="https://cdn.jsdelivr.net/gh/semicon/cdn/web3.0/webapps/libs/js/bootstrap-parts@0.1.1.min.js"><\/script>`); 
 if (!linkx) { document.write(`<div id="cover" style="position:fixed; top:0; left:0; background:rgba(0,0,0,0.95); z-index:99999; width:100%; height:100%;"><div style="position: absolute;  left: 50%;  top: 50%; z-index:15; color:red; transform: translate(-50%, -50%);"><center><h2>Sorry!, you are violating the terms.</h2></center></div></div>`);} 
 const divfab = ex.innerHTML.match(`<div id="fab-active"><\/div>`);
-if (!divfab) { document.write(`<div id="cover" style="position:fixed; top:0; left:0; background:rgba(0,0,0,0.95); z-index:99999; width:100%; height:100%;"><div style="position: absolute;  left: 50%;  top: 50%; z-index:15; color:red; transform: translate(-50%, -50%);"><center><h2>Sorry!, you are violating the terms.</h2></center></div></div>`);} 
+if (!divfab) { document.write(`<div id="cover" style="position:fixed; top:0; left:0; background:rgba(0,0,0,0.95); z-index:99999; width:100%; height:100%;"><div style="position: absolute;  left: 50%;  top: 50%; z-index:15; color:red; transform: translate(-50%, -50%);"><center><h2>Sorry!, you are violating the terms.</h2></center></div></div>`);}
+`)
